4a6c64e8434764c90891178e6ed50ed4ab4fc86f
Keerthan <keerthangowda618.com>
2026-05-05 20:29:12 +0530

    Initial commit for pixii

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-celery -A config worker -l info --concurrency=1 &
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers=2
+celery -A config worker -l info -P solo &
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers=1
